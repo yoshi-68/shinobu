deea82b119ac2297d1e9ff04633515446508e808
Yoshi_F <yoshi.android@gmail.com>
2022-10-20 20:16:10 +0900

    prettier-plugin-sort-importsを追加

--- prettier.config.cjs
+++ prettier.config.cjs
@@ -1,6 +1,10 @@
 module.exports = {
+    printWidth: 80,
     trailingComma: "es5",
     tabWidth: 4,
     semi: true,
     singleQuote: false,
+    importOrder: ["^@core/(.*)$", "^@server/(.*)$", "^@ui/(.*)$", "^[./]"],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true
 };
