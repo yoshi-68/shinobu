c5e346c1998e1ea0beec78473392cc00fa19926d
Yoshi_F <yoshi.android@gmail.com>
2022-10-23 20:38:44 +0900

    interfaceからtypeへ変更

--- electron/@types/types.d.ts
+++ electron/@types/types.d.ts
@@ -1,9 +1,16 @@
 // declare module "types" {
-export interface charaData {
+// export interface charaData {
+//     id: number;
+//     name: string;
+//     iconPath: string;
+//     type: string;
+//     position: number;
+// }
+export type charaData = {
     id: number;
     name: string;
     iconPath: string;
     type: string;
     position: number;
-}
+};
 // }
