809b56ec4ca29fe19885f7260d0d04dc45e5c9bf
Yoshi_F <yoshi.android@gmail.com>
2022-10-30 00:02:22 +0900

    キャラ検索を新規追加

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import CharaSelect from "./components/CharaSelect";
+import CharaSearch from "./components/CharaSearch";
 import "./css/App.css";
 
 function App() {
     return (
         <div className="App">
             <CharaSelect />
+            <CharaSearch />
         </div>
     );
 }
---
+++ src/components/CharaSearch.tsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+class CharaSearch extends Component {
+    render() {
+        return (
+            <>
+                <p>aaa</p>
+            </>
+        );
+    }
+}
+
+export default CharaSearch;
