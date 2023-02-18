19a3cee67ae1beca1ed8e00bb901eac4216f08a3
Yoshi_F <yoshi.android@gmail.com>
2023-02-19 01:11:26 +0900

    JSDocの追記

--- electron/settings.ts
+++ electron/settings.ts
@@ -1,9 +1,28 @@
+/**
+ * ログファイルのサイズ
+ */
 export const MAXIMUM_LOG_FILE_SIZE = 524288;
+
+/**
+ * キャラクタデータの取得先
+ */
 export const GET_CHARA_DATA_URL = 'https://appmedia.jp/priconne-redive/4466131';
+
+/**
+ * 防衛突破編成の検索先
+ */
 export const FEATCH_ORGANIZATIONS =
   'https://fe-app.matomeroyal.com/wp-content/themes/stingerplus-child/api/priconne/priconne_select.php';
+
+/**
+ * UserAgentの設定
+ */
 export const USER_AGENT =
   'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36';
+
+/**
+ * キャラクタのポジション分け
+ */
 export const FORWARD_GUARD = '前衛';
 export const MIDDLE_GUARD = '中衛';
 export const BACKWARD_GUARD = '後衛';
